35cb8be960d5be1be1ac8ef6c06d951318cca4da
vison-v <71822883+vison-v@users.noreply.github.com>
2021-01-01 23:00:35 +0800

    Update common.sh

--- Lienol/common/common.sh
+++ Lienol/common/common.sh
@@ -43,7 +43,9 @@ sed -i 's/msgstr "诊断"/msgstr "网络诊断"/g' feeds/luci/applications/luci-
 
 sed -i 's/Lienol"/Master"/g' feeds/luci/luci.mk
 
-#sed -i '/wifi up/a\date=`date +%m.%d.%Y`' package/default-settings/files/zzz-default-settings
+date=`date +%m.%d.%Y`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C Build By ViS0N'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 #sed -i "s/'OpenWrt SNAPSHOT '/'OpenWrt SNAPSHOT $date Build By VIS0N '/g" package/default-settings/files/zzz-default-settings
 
 sed -i 's/msgstr "Socat"/msgstr "端口转发"/g' feeds/lienol/luci-app-socat/po/zh-cn/socat.po
