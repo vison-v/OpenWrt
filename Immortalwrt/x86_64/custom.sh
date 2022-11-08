03dfe7d39e2f2b940079bcf9fe5082e7bf1a69c7
vison-v <71822883+vison-v@users.noreply.github.com>
2022-11-08 15:33:27 +0800

    Create custom.sh

---
+++ Immortalwrt/x86_64/custom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+for e in $(ls -d package/openwrt-packages/luci-app-*/po); do
+	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
+		ln -s zh-cn $e/zh_Hans 2>/dev/null
+	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
+		ln -s zh_Hans $e/zh-cn 2>/dev/null
+	fi
+done
