f24d00ca33ab0be4b19eee25cfc1e41d86a36700
vison-v <71822883+vison-v@users.noreply.github.com>
2022-11-09 00:47:36 +0800

    Update custom.sh

--- Immortalwrt/x86_64_docker/custom.sh
+++ Immortalwrt/x86_64_docker/custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-for e in $(ls -d 1/luci-app-*/po); do
+for e in $(ls -d package/openwrt-packages/luci-app-*/po); do
 	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
 		ln -s zh-cn $e/zh_Hans 2>/dev/null
 	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
