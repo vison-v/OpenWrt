2980f022bcddb44f6092f321d0cbafab6ecfcf6e
vison-v <71822883+vison-v@users.noreply.github.com>
2022-11-09 23:26:24 +0800

    Update common.sh

--- Immortalwrt/common/common.sh
+++ Immortalwrt/common/common.sh
@@ -125,6 +125,7 @@ sed -i 's/上网时间控制/时间控制/g' feeds/luci/applications/luci-app-ac
 sed -i 's/services/control/g'  `grep services -rl feeds/luci/applications/luci-app-accesscontrol/luasrc`
 
 sed -i 's/Tcpdump 流量监控/流量监控/g' package/openwrt-packages/luci-app-tcpdump/po/zh-cn/tcpdump.po
+sed -i 's/"Tcpdump"/"流量监控"/g' package/openwrt-packages/luci-app-tcpdump/luasrc/controller/tcpdump.lua
 
 sed -i 's/services/control/g'  `grep network -rl package/openwrt-packages/OpenAppFilter/luci-app-oaf/luasrc`
 
