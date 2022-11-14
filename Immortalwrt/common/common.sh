89b3f964c126f1ed3882e119f5989ef4e3a51562
vison-v <71822883+vison-v@users.noreply.github.com>
2022-11-14 08:37:37 +0800

    Update common.sh

--- Immortalwrt/common/common.sh
+++ Immortalwrt/common/common.sh
@@ -7,9 +7,9 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/oaf package/openwrt-pac
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dnsfilter package/openwrt-packages/luci-app-dnsfilter
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/openwrt-packages/luci-app-bypass
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/openwrt-packages/luci-app-adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/openwrt-packages/adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/openwrt-packages/luci-app-smartdns
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/openwrt-packages/smartdns
+svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/openwrt-packages/adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-smartdns package/openwrt-packages/luci-app-smartdns
+svn co https://github.com/kiddin9/openwrt-packages/trunk/smartdns package/openwrt-packages/smartdns
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-aliyundrive-webdav package/openwrt-packages/luci-app-aliyundrive-webdav
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-aliyundrive-fuse package/openwrt-packages/luci-app-aliyundrive-fuse
 
