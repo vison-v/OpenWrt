09ff7d1e3f480554b777e80e315d2bf5fe30ef93
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-02 23:47:38 +0800

    Update common.sh

--- Immortalwrt/common/common.sh
+++ Immortalwrt/common/common.sh
@@ -41,11 +41,10 @@ function mvdir() {
 mkdir package/openwrt-packages
 ##--------------------------##
 git_sparse_clone main "https://github.com/kiddin9/kwrt-packages.git" luci-app-tcpdump
-git_sparse_clone master "https://github.com/kenzok8/small.git" luci-app-passwall
+git clone https://github.com/kenzok8/small.git && mv small/* package/openwrt-packages/
 #git_sparse_clone master "https://github.com/lunatickochiya/Matrix-Action-Openwrt" package/kochiya/brlaser package/kochiya/luci-app-banmac-ipt package/kochiya/luci-app-banmac-nft package/kochiya/luci-app-nvr package/kochiya/luci-app-openvpn-server
 ##--------------------------##
 mv luci-app-tcpdump package/openwrt-packages/
-mv luci-app-passwall package/openwrt-packages/
 ##############转换app语言包################
 #curl -fsSL  https://raw.githubusercontent.com/vison-v/OpenWrt/main/Immortalwrt/common/custom.sh >> package/openwrt-packages/custom.sh
 #chmod +x package/openwrt-packages/custom.sh && bash package/openwrt-packages/custom.sh
