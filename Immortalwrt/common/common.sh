a1f85bbf0de79dc539f4eb0be290b20a512f010e
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-02 18:35:27 +0800

    Update common.sh

--- Immortalwrt/common/common.sh
+++ Immortalwrt/common/common.sh
@@ -152,7 +152,7 @@ sed -i 's/services/control/g'  `grep network -rl feeds/luci/applications/luci-ap
 
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
 
-sed -i 's/network/control/g' feeds/luci/applications/luci-app-eqos/root/usr/share/luci/menu.d/luci-app-eqos.json
+sed -i 's/services/control/g' feeds/luci/applications/luci-app-eqos/root/usr/share/luci/menu.d/luci-app-eqos.json
 sed -i '/msgid "EQoS"/{n;s/IP限速/网速控制/;}' feeds/luci/applications/luci-app-eqos/po/zh_Hans/eqos.po
 
 sed -i 's/CPU 性能优化调节/CPU 调节/g' feeds/luci/applications/luci-app-cpufreq/po/zh_Hans/cpufreq.po
