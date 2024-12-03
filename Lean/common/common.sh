842002d27549432ecfdb77370c41b1ae8825c3f3
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-03 23:42:30 +0800

    Update common.sh

--- Lean/common/common.sh
+++ Lean/common/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-rm -Rf feeds/packages/net/{smartdns,aria2,adguardhome,wrtbwmon} feeds/packages/lang/golang feeds/luci/themes/luci-theme-argon feeds/luci/applications/{luci-app-netdata,luci-app-baidupcs-web,luci-app-dockerman,luci-app-wrtbwmon,luci-app-watchcat,luci-app-ttyd,luci-app-unblockmusic,luci-app-pushbot}
+rm -Rf feeds/packages/net/{smartdns,aria2,adguardhome,wrtbwmon} feeds/packages/lang/golang feeds/luci/themes/luci-theme-argon feeds/luci/applications/{luci-app-eqos,luci-app-netdata,luci-app-baidupcs-web,luci-app-dockerman,luci-app-wrtbwmon,luci-app-watchcat,luci-app-ttyd,luci-app-unblockmusic,luci-app-pushbot}
 ##############定义函数################
 function git_clone() {
           git clone --depth 1 $1 $2
@@ -52,7 +52,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-pack
 #git clone https://github.com/pymumu/luci-app-smartdns -b lede package/openwrt-packages/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git package/openwrt-packages/smartdns
 
-git_sparse_clone main "https://github.com/kiddin9/kwrt-packages.git" wrtbwmon luci-app-wrtbwmon luci-app-watchcat luci-app-ttyd luci-app-unblockmusic luci-app-pushbot luci-app-smartdns luci-app-tcpdump && mv -n wrtbwmon package/openwrt-packages/
+git_sparse_clone main "https://github.com/kiddin9/kwrt-packages.git" luci-app-eqos wrtbwmon luci-app-wrtbwmon luci-app-watchcat luci-app-ttyd luci-app-unblockmusic luci-app-pushbot luci-app-smartdns luci-app-tcpdump && mv -n wrtbwmon package/openwrt-packages/
 git_sparse_clone main "https://github.com/kiddin9/luci.git" applications/luci-app-ttyd
 
 git_clone https://github.com/Lienol/openwrt-package.git && mv -n openwrt-package/{luci-app-control-timewol,luci-app-control-webrestriction,luci-app-control-weburl} package/openwrt-packages/; rm -rf openwrt-package
