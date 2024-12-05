71e86c4e26911eb1bc2bd47d68122cc11d4d1da0
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-05 23:28:51 +0800

    Update common.sh

--- Lean/common/common.sh
+++ Lean/common/common.sh
@@ -134,7 +134,7 @@ sed -i 's/services/control/g'  `grep services -rl package/openwrt-packages/OpenA
 sed -i 's/"Argon 主题设置"/"主题设置"/g' package/openwrt-packages/luci-app-argon-config/po/zh_Hans/argon-config.po
 #sed -i 's/TTYD 终端/命令终端/g' package/openwrt-packages/luci-app-ttyd/po/zh_Hans/terminal.po
 sed -i 's/services/vpn/g'  package/openwrt-packages/luci-app-wechatpush/root/usr/share/luci/menu.d/luci-app-wechatpush.json
-sed -i 's/control/services/g'  `grep control -rl package/openwrt-packages/luci-app-control-timewol/luasrc`
+sed -i 's/"control"/"services"/g'  `grep control -rl package/openwrt-packages/luci-app-control-timewol/luasrc`
 
 sed -i '/msgid "Pass Wall"/{n;s/PassWall/翻越长城/;}' package/openwrt-packages/small/luci-app-passwall/po/zh_Hans/passwall.po
 sed -i '/Pass Wall/s/-1/4/g' package/openwrt-packages/small/luci-app-passwall/luasrc/controller/passwall.lua
