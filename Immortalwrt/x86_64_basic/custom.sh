56e095dad165978c7c3d913c1e1f679db5d8116c
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-17 18:56:21 +0800

    Update custom.sh

--- Immortalwrt/x86_64_basic/custom.sh
+++ Immortalwrt/x86_64_basic/custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##############用nginx替换uhpptd################
 sed -i 's/+luci-light/+luci-nginx/g' feeds/luci/collections/luci/Makefile
-echo "---------设置完成---------"
+echo -----------------FINISH-------------------
