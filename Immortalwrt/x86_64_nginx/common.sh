7f1c8a60043082768d4083c177fbca7f65b0de0a
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-17 13:31:11 +0800

    Update common.sh

--- Immortalwrt/x86_64_nginx/common.sh
+++ Immortalwrt/x86_64_nginx/common.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ##############用nginx替换uhpptd################
 rm -rf feeds/luci/collections/luci-light/Makefile && curl -fsSL  https://raw.githubusercontent.com/vison-v/patches/main/Makefile >> feeds/luci/collections/luci-light/Makefile
+echo "---------设置完成---------"
