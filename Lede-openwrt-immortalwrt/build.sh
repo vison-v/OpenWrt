1e1f2f52782db01ccff3a031d11ac80b1b3967db
vison-v <71822883+vison-v@users.noreply.github.com>
2024-12-29 16:50:04 +0800

    Update build.sh

--- Lede-openwrt-immortalwrt/build.sh
+++ Lede-openwrt-immortalwrt/build.sh
@@ -51,16 +51,16 @@ esac
 
 # 克隆或更新仓库  
 if [ ! -d "${CONFIG_REPO}" ]; then    
-  git clone --depth=1 -b "${REPO_BRANCH}" "${REPO_URL}" "${CONFIG_REPO}" || {  
-    echo "Failed to clone repository."  
+  git clone --depth=1 -b "${REPO_BRANCH}" "${REPO_URL}" "openwrt" || {  
+    echo "克隆仓库失败！"  
     exit 1  
   }  
 fi  
 
 # root.  
 export FORCE_UNSAFE_CONFIGURE=1  
-pushd "${CONFIG_REPO}" || exit 1  
-git pull || { echo "Failed to pull latest changes."; exit 1; }  
+pushd openwrt || exit 1  
+git pull || { echo "未能拉取最新更改！"; exit 1; }  
 
 # Update feeds and install packages  
 sed -i "/src-git vi /d; 1 i src-git vi https://github.com/vison-v/packages;${CONFIG_REPO}" feeds.conf.default  
@@ -95,43 +95,6 @@ if [ "$GITHUB_ACTIONS" = "true" ]; then
   popd || exit 1  
 fi  
 
-echo "Downloading packages..."  
+echo "下载packages..."  
 make download -j8 V=s  
-
-# 并行构建设置  
-echo "开始编译${CONFIG_REPO}-${CONFIG_NAME}固件用 $(nproc) 线程..."  
-
-# 设置错误日志路径
-ERROR_LOG_NAME="${CONFIG_REPO}-${CONFIG_NAME}_make_error.log"  
-
-# 执行make命令，若失败返回错误码1并记录错误日志路径  
-(make -j$(nproc) V=s > make_output.log 2> "${ERROR_LOG_NAME}") || \
-(make -j1 V=s >> make_output.log 2>> "${ERROR_LOG_NAME}")  
-
-if [ $? -ne 0 ]; then  
-  echo "BUILD_STATUS=failed" >> $GITHUB_ENV   # 失败时设置状态变量  
-  echo "${CONFIG_REPO}-${CONFIG_NAME}固件构建失败!"  
-  echo "${CONFIG_REPO}-${CONFIG_NAME}构建错误日志:"  
-  cat "${ERROR_LOG_NAME}"  
-  mv -f "${ERROR_LOG_NAME}" "${WORKSPACE}"  
-  exit 1    
-else  
-  echo "BUILD_STATUS=success" >> $GITHUB_ENV  # 成功时设置状态变量  
-  echo "${CONFIG_REPO}-${CONFIG_NAME}固件构建成功!"  
-fi  
-ls -al  
-
-# 移动构建产物  
-pushd bin/targets/*/* || exit 1  # 确保目录变更成功  
-ls -al  
-# 移动文件到工作空间  
-if ls *combined*.img.gz 1> /dev/null 2>&1; then  
-  mv -f *combined*.img.gz "${WORKSPACE}"  
-else  
-  echo "没有发现编译好的固件！"  
-  exit 1  
-fi  
 popd || exit 1  # 确保返回成功  
-popd || exit 1  # 确保返回成功  
-du -chd1 "${CONFIG_REPO}"  
-echo "完成"
