319a9ecd64469eb95c7b707cb2a76630b4e7b344
vison-v <71822883+vison-v@users.noreply.github.com>
2025-01-25 01:10:24 +0800

    Update feeds-vi.sh

--- lede/common/feeds-vi.sh
+++ lede/common/feeds-vi.sh
@@ -7,7 +7,8 @@ echo "Starting to find subdirectories in feeds/vi and delete matching directorie
 find "feeds/vi" -maxdepth 1 -type d -printf '%f\0' | while IFS= read -r -d '' sub_dir; do
     echo "Processing subdirectory: $sub_dir"
     # 执行查找并删除操作
-    find "feeds" -depth -path "vi" -prune -o -type d -name "$sub_dir" -exec rm -rf {} +  
+    #find "feeds" -depth -path "vi" -prune -o -type d -name "$sub_dir" -exec rm -rf {} +  
+    find "feeds" -depth -path "feeds/vi" -prune -o -type d -name "$sub_dir" -print
     # 输出查找并删除操作完成信息
     echo "Finished processing subdirectory: $sub_dir"
 done
