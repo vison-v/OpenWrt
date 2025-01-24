4381c97d220c4b39ea0b2f5863a5a3ad75d5442f
vison-v <71822883+vison-v@users.noreply.github.com>
2025-01-24 14:42:12 +0800

    Update feed.sh

--- lede/common/feed.sh
+++ lede/common/feed.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 开启调试模式，输出每个执行的命令
-set -x
-
 # 输出开始信息
 echo "Starting to find subdirectories in feeds/vi and delete matching directories in /"
 
@@ -10,7 +7,8 @@ echo "Starting to find subdirectories in feeds/vi and delete matching directorie
 find "feeds/vi" -maxdepth 1 -type d -printf '%f\0' | while IFS= read -r -d '' sub_dir; do
     echo "Processing subdirectory: $sub_dir"
     # 执行查找并删除操作
-    find "/" -path "feeds/vi" -prune -o -type d -name "$sub_dir" -delete
+    #find "/" -path "feeds/vi" -prune -o -type d -name "$sub_dir" -delete
+    find "/" -depth -path "feeds/vi" -prune -o -type d -name "$sub_dir" -print -delete
     # 输出查找并删除操作完成信息
     echo "Finished processing subdirectory: $sub_dir"
 done
@@ -18,5 +16,3 @@ done
 # 输出结束信息
 echo "Script execution completed."
 
-# 关闭调试模式
-set +x
