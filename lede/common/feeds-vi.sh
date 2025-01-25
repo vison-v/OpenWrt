0ac2bc2a2e7f7d56c809b2493416b4ea9b2e60f4
vison-v <71822883+vison-v@users.noreply.github.com>
2025-01-25 14:49:19 +0800

    Update feeds-vi.sh

--- lede/common/feeds-vi.sh
+++ lede/common/feeds-vi.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
 # 输出开始信息
-echo "开始在 feeds/vi 目录中查找子目录，并替换 feeds 目录中匹配的目录.................."
+echo "开始在 feeds/vi 目录中查找子目录，并删除 feeds 目录中匹配的目录.................."
 
 # 获取 feeds/vi 目录下的子目录名
 find "feeds/vi" -maxdepth 1 -type d \( ! -name "vi" -a ! -name ".git" \) -printf '%f\0' | while IFS= read -r -d '' sub_dir; do
     echo "需要处理的目录为: $sub_dir"
-    find "feeds" -maxdepth 3 -type d -name "$sub_dir" ! -path "feeds/vi*" -print0 | while IFS= read -r -d '' target_dir; do
-        source_dir="feeds/vi/$sub_dir"
-        if [ -d "$source_dir" ]; then
-            # 创建临时目录
-            temp_dir=$(mktemp -d)
-            # 将源目录内容递归移动到临时目录
-            mv "$source_dir/"* "$temp_dir/"
-            # 删除目标目录
-            rm -rf "$target_dir"
-            # 将临时目录内容移动到目标目录位置
-            mv "$temp_dir" "$target_dir"
-            echo "已将 $source_dir 替换到 $target_dir"
-        else
-            echo "源目录 $source_dir 不存在，跳过替换操作"
-        fi
-    done
+    # 执行查找并删除操作
+    find "feeds" -maxdepth 3 -type d -name "$sub_dir" ! -path "feeds/vi*" -print0 | xargs -0 -I {} bash -c 'echo "删除: {}"; rm -rf {}'
+    # 输出查找并删除操作完成信息
     echo "完成目录处理: $sub_dir"
 done
 
