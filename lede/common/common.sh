2303500732a408ef73e7bf18322e856481ce8794
vison-v <71822883+vison-v@users.noreply.github.com>
2025-01-27 11:50:07 +0800

    Update common.sh

--- lede/common/common.sh
+++ lede/common/common.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
-sed -i '19,34d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/aria2.lua/,/samba4.json/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/msgid "Hostnames"/{n;s/主机名/主机映射/;}' feeds/luci/modules/luci-base/po/zh_Hans/base.po
 sed -i 's/LEDE /LEDE Build By ViS0N /' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/msgstr "诊断"/msgstr "网络诊断"/g' feeds/luci/applications/luci-app-diag-core/po/zh_Hans/diag_core.po
